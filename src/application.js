34e667d6ccd2d8622e2c11532f9c60b391942830
Zcyon <lascariopacheco@hotmail.com>
2014-12-05 11:31:59 -0430

    Created the basic interface and tools to develop the application

---
+++ src/application.js
@@ -0,0 +1,6 @@
+var loadCanvas = function () {
+	"use strict";
+	
+	var canvas = new HiCanvas(800, 300);
+	canvas.addToObj('canvasWrapper');
+};
---
+++ src/responses.js
@@ -0,0 +1,17 @@
+var showCanvas = function () {
+	$('#canvasWrapper').fadeIn();
+};
+
+var hideCanvas = function () {
+	$('#canvasWrapper').fadeOut();
+};
+
+var showHideButton = function () {
+	"use strict";
+	$('#hideButton').fadeIn();
+};
+
+var hideHideButton = function () {
+	"use strict";
+	$('#hideButton').fadeOut();
+};
